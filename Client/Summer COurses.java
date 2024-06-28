ea067433f527547c0bebb3d6ed8ba37116b48f81
psg0009 <139065268+psg0009@users.noreply.github.com>
2024-06-28 16:18:34 +0530

    Client side

---
+++ Client/Summer COurses.java
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <meta charset=utf-8" />
+  <title>DOM Objects in JavaScript</title>
+ </head>
+ <body>
+  <h3>DOM Objects in JavaScript</h3>
+  <p id="par1">Text for first paragraph</p>
+  <a href="#" id="achor1">Null link</a>
+ </body>
+</html>
